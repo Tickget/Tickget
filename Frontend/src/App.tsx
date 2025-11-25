d73d023d516d6793c22675193a2216c7bb196ab7
DESKTOP-HA97GMI\SSAFY <h2sorginal@gmail.com>
2025-11-25 23:31:36 +0900

    fix: github CI 테스트 주석

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { router } from "./app/routes";
 import { AlertProvider } from "./shared/providers/AlertProvider";
 
 export default function App() {
+  // gitHub CI test 주석
   return (
     <AlertProvider>
       <RouterProvider router={router} />
